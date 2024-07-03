25d047aba1e9923d1568957a1da3b169d9ba67cb
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-07-03 15:02:17 -0400

    Update config.py

--- config.py
+++ config.py
@@ -23,3 +23,24 @@ def update_config(file: str = __CONFIG_FILE__, configuration: dict[str, str] = {
         for key, value in c.items():
             config.write(f'{key}="{value}"\n')
 
+def revert_to_default(file: str = __CONFIG_FILE__) -> None:
+    with open(file, 'w') as config:
+        config.write(
+            'key_file = "key.key"\n'
+            'token_vault = "token.vault"\n'
+            'name = "***** ****"\n'
+            'email = "*******@gmail.com"\n'
+            'local_repo_path = "./github_local"\n'
+            'remote_url = "*******************************"\n'
+            'logfile = "logfile"\n'
+            'err = "errInfo.csv"\n'
+        )
+
+def default_key_filename(key_file: str | None = None, filename: str | None = None):
+    configuration = get_config()
+    if key_file is None:
+        key_file = configuration['key_file']
+    if filename is None:
+        filename = configuration['token_vault']
+    return key_file, filename
+
