07f55036782ec1b7c8fca26d3330cb364fdc614f
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-07-03 15:01:54 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,17 @@
 import os
 
-__CONFIG_FILE__ = "config"
+__CONFIG_FILE__: str = "config"
+
+def get_config(file: str = __CONFIG_FILE__) -> dict[str, str]:
+    if not os.path.exists(file):
+        revert_to_default(file=file)
+    configuration = {}
+    with open(file, 'r') as config:
+        lines = config.read().split('\n')
+    for line in lines:
+        if len(line) < 5 or len(line.split('"')) != 3:
+            continue
+        key = line.split(' ')[0]
+        configuration[key] = line.split('"')[1]
+    return configuration
+
